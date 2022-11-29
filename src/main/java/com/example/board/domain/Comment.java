4df4f2f73cc7b533cc0289c09f7e28c12c618d49
leewg97 <leewg97@naver.com>
2022-11-30 07:43:32 +0900

    Feat #3 : Convert domain code to entity

--- src/main/java/com/example/board/domain/Comment.java
+++ src/main/java/com/example/board/domain/Comment.java
@@ -1,17 +1,78 @@
 package com.example.board.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString(callSuper = true)
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
     private Article article;
+
+    @Setter
+    @Column(nullable = false, length = 500)
     private String content;
 
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
-    private String cratedBy;
+
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
 
+    protected Comment() {}
+
+    public Comment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static Comment of(Article article, String content) {
+        return new Comment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment comment)) return false;
+        return id != null && id.equals(comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 }
