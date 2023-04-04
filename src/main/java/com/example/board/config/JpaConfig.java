87e0e4cd0b64ded0e5436a9b9252bc3871aa3cb8
leewg97 <leewg97@naver.com>
2023-04-04 15:14:41 +0900

    Feat #29 : 시큐리티 설정과 자바 대응 코드 구현 * 스프링 부트 2.7 과 가이드에 맞춰서 시큐리티 설정을 적용하고, 인증 정보를 db로부터 부르는 빈과 패스워드 인코더 빈까지 등록

--- src/main/java/com/example/board/config/JpaConfig.java
+++ src/main/java/com/example/board/config/JpaConfig.java
@@ -1,9 +1,13 @@
 package com.example.board.config;
 
+import com.example.board.dto.security.BoardPrincipal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -13,7 +17,12 @@ public class JpaConfig {
 
     @Bean   // 사람 이름 정보를 넣기 위한 설정
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.of("Lee");    // TODO: Spring Security로 인증 구현 시 수정
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(BoardPrincipal.class::cast)
+                .map(BoardPrincipal::getUsername);
     }
 
 }
